124d1474a05b6333805a6ecf4a321a32b41554f6
yeison <yeisonkb@gmail.com>
2022-11-12 18:28:59 -0500

    starting project- crud and openAPI settings

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mi API documentation')
+    .setDescription('Pruebas')
+    .setVersion('1.0')
+    .addTag('items')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
